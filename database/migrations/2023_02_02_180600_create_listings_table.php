c85e51d762e6f561b9a0b54890e3620f68f8bc31
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-02-02 13:18:19 -0500

    Created Listing Model and migrated new database to store listings

---
+++ database/migrations/2023_02_02_180600_create_listings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id('listings_id');
+            $table->integer('listings_user_id');
+            $table->string('listings_name');
+            $table->string('listings_adress');
+            $table->string('listings_website');
+            $table->string('listings_email');
+            $table->integer('listings_phone')->unsigned();
+            $table->string('listings_bio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+};
