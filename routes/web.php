753c729c169430511bc1fecc71346ff39b85214a
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-02-02 12:58:57 -0500

    Created controller for listings, added route for listings

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//Created resource route
+Auth::resource('listings', 'ListingsController');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
