a67dd4da75fe8c0eee4352ff3ce0c3e0debdc27e
Matthew Lowe <Matthewjlowe11@gmail.com>
2023-02-04 14:15:23 -0500

    Added success and error message include

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -16,28 +15,28 @@
                                     @CSRF
                                     <div class="form-group">
                                     <label for="name">Name</label>
-                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" required>
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
                                     </div>
                                     <div class="form-group">
                                     <label for="email">Email address</label>
-                                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+                                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
                                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                     </div>
                                     <div class="form-group">
                                     <label for="address">Subject</label>
-                                    <input type="text" class="form-control" id="address" name="address" placeholder="Enter address" required>
+                                    <input type="text" class="form-control" id="address" name="address" placeholder="Enter address">
                                     </div>
                                     <div class="form-group">
                                     <label for="website">Website</label>
-                                    <input type="text" class="form-control" id="website" name="website" placeholder="Enter website" required>
+                                    <input type="text" class="form-control" id="website" name="website" placeholder="Enter website">
                                     </div>
                                     <div class="form-group">
                                     <label for="address">Phone</label>
-                                    <input type="number" class="form-control" id="phone" name="phone" placeholder="Enter phone" required>
+                                    <input type="number" class="form-control" id="phone" name="phone" placeholder="Enter phone">
                                     </div>
                                     <div class="form-group">
                                     <label for="bio">Listing Bio</label>
-                                    <input type="text" class="form-control" id="bio" name="bio" placeholder="Enter bio" required>
+                                    <input type="text" class="form-control" id="bio" name="bio" placeholder="Enter bio">
                                     </div>
                                     <button type="submit" class="btn btn-primary mt-2" style="float:right">Submit</button>
                                 </form>
@@ -46,6 +45,5 @@
                 </div>
             </div>
         </div>
-    </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,10 @@
         @include('inc.nav')
 
         <main class="py-4">
-            @yield('content')
+            <div class="container">
+                @include('inc.messages')
+                @yield('content')
+            </div>
         </main>
     </div>
 </body>
